9ebcfec1764d560f4970f24080b41da02b009820
Edgar Acosta <atsoca_ragde@hotmail.com>
2026-02-28 19:03:32 -0600

    feat: primer commit

---
+++ src/test/java/com/robot/pages/LoginPage.java
@@ -0,0 +1,31 @@
+package com.robot.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    WebDriver driver;
+
+    // 1. Localizadores (Nuestros "objetos")
+    By txtUsername = By.id("user-name");
+    By txtPassword = By.id("password");
+    By btnLogin = By.id("login-button");
+
+    // 2. Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // 3. Acciones (Nuestros "servicios")
+    public void escribirUsuario(String usuario) {
+        driver.findElement(txtUsername).sendKeys(usuario);
+    }
+
+    public void escribirPassword(String password) {
+        driver.findElement(txtPassword).sendKeys(password);
+    }
+
+    public void clickEnLogin() {
+        driver.findElement(btnLogin).click();
+    }
+}
---
+++ src/test/java/com/robot/tests/LoginTest.java
@@ -0,0 +1,56 @@
+package com.robot.tests;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.robot.pages.LoginPage;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        
+        Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);
+        
+        // CONFIGURACIÓN PARA LINUX/CI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        // Descomenta la siguiente línea para correr sin ver la ventana (obligatorio en Jenkins)
+        // options.addArguments("--headless"); 
+
+        driver = new ChromeDriver(options);
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void validarLoginExitoso() {
+        LoginPage login = new LoginPage(driver); // Instanciamos la página
+        login.escribirUsuario("standard_user");
+        login.escribirPassword("secret_sauce");
+        login.clickEnLogin();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
